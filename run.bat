74857e6125448ab473ce54f314a79ed32808811b
Norayr Petrosyan <nordeveloper@gmail.com>
2026-07-05 16:11:10 +0400

    --

--- run.bat
+++ run.bat
@@ -1,16 +1,13 @@
 @echo off
 
-echo [1/4] Stopping any running instances...
+echo [1/3] Stopping any running instances...
 taskkill /F /IM node.exe >nul 2>&1
 
 echo.
-echo [2/4] Running the application...
-call npm run dev
+echo [2/3] Running the application...
+call npm run start
 
 echo.
-echo [3/4] Application is running in development mode.
-
-echo.
-echo [4/4] To stop the application, press Ctrl+C.
+echo [3/3] To stop the application, press Ctrl+C.
 
 pause
